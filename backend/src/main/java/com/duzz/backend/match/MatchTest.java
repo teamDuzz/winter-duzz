bb9aebdec808a9b636cfd524eb84c2fd1b4c7392
jungwoo <gjw1972@naver.com>
2025-01-03 22:07:02 +0900

    :arrow_up: 24->48

--- backend/src/main/java/com/duzz/backend/match/MatchTest.java
+++ backend/src/main/java/com/duzz/backend/match/MatchTest.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 public class MatchTest {
     public static void main(String[] args) {
-        Mentor mentor1 = new Mentor("kim", new int[7][24], "컴퓨터공학과", Arrays.asList("자바", "파이썬"));
-        Mentor mentor2 = new Mentor("박멘토", new int[7][24], "전자공학과", Arrays.asList("C", "C++"));
-        Mentor mentor3 = new Mentor("이멘토", new int[7][24], "경영학과", Arrays.asList("경영학", "마케팅"));
-        Mentor mentor4 = new Mentor("최멘토", new int[7][24], "수학과", Arrays.asList("수학", "통계학"));
-        Mentee mentee1 = new Mentee("김멘티", new int[7][24], Arrays.asList("자바", "파이썬"));
+        Mentor mentor1 = new Mentor("kim", new int[7][48], "컴퓨터공학과", Arrays.asList("자바", "파이썬"));
+        Mentor mentor2 = new Mentor("박멘토", new int[7][48], "전자공학과", Arrays.asList("C", "C++"));
+        Mentor mentor3 = new Mentor("이멘토", new int[7][48], "경영학과", Arrays.asList("자바", "파이썬"));
+        Mentor mentor4 = new Mentor("최멘토", new int[7][48], "수학과", Arrays.asList("수학", "통계학"));
+        Mentee mentee1 = new Mentee("김멘티", new int[7][48], Arrays.asList("자바", "파이썬"));
         mentor1.schedule[0][9] = 1;
         mentor1.schedule[0][10] = 1;
         mentor1.schedule[0][11] = 1;
--- backend/src/main/java/com/duzz/backend/match/Mentor.java
+++ backend/src/main/java/com/duzz/backend/match/Mentor.java
@@ -8,7 +8,7 @@ public class Mentor extends Profile {
     private String major;
     List<String>subjects;
     public Mentor() {
-        super("", new int[5][24]);
+        super("", new int[5][48]);
     }
     public Mentor(String name, int[][] schedule, String major, List<String> subjects) {
         super(name, schedule);
